44b7d4f32a5fd15a6925f6427fe1d7c823771fd5
gracenak <gracenak@gmail.com>
2020-10-09 01:14:53 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
-  end
+    user = User.new(:username => params[:username], :password => params[:password])
+	   
+		if user.save
+		  redirect "/login"
+		else
+		  redirect "/failure"
+		end
+	end
 
   get '/account' do
     @user = User.find(session[:user_id])
